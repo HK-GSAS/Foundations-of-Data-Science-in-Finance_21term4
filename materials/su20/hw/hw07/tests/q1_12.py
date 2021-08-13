0eb681a399cc07f5772487b0f81656c84e91c404
juliantandy <84844991+juliantandy@users.noreply.github.com>
2021-08-13 18:06:37 +0800

    Add files via upload

--- materials/su20/hw/hw07/tests/q1_12.py
+++ materials/su20/hw/hw07/tests/q1_12.py
@@ -1,22 +1,3 @@
-test = {
-  'name': 'q1_12',
-  'points': 1,
-  'suites': [
-    {
-      'cases': [
-        {
-          'code': r"""
-          >>> 0 <= p_value < 1
-          True
-          """,
-          'hidden': False,
-          'locked': False
-        }
-      ],
-      'scored': True,
-      'setup': '',
-      'teardown': '',
-      'type': 'doctest'
-    }
-  ]
-}
+test = {   'name': 'q1_12',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> len(visited_test_statistics_under_null) == 20000\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
